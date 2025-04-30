53652a00fdba342d69e9ab9823cd984cc3a99bf3
Kaustubh Kale <s224942922@deakin.edu.au>
2025-04-30 21:43:17 +1000

    Pushing task 7.2P

--- Task 7.2P/public/scripts.js
+++ Task 7.2P/public/scripts.js
@@ -1,7 +1,22 @@
-// Connect to the server socket
 const socket = io();
 
 socket.on('number', (msg) => {
     console.log('Random number:', msg);
     document.getElementById('number').innerText = msg;
+});
+
+function sendMessage() {
+    const input = document.getElementById('text');
+    const msg = input.value.trim();
+    if (msg) {
+        socket.emit('message', msg);
+        input.value = '';
+    }
+}
+
+socket.on('message', (msg) => {
+    const messagesDiv = document.getElementById('messages');
+    const newMsg = document.createElement('li');
+    newMsg.innerText = msg;
+    messagesDiv.appendChild(newMsg);
 });
--- Task 7.2P/server.js
+++ Task 7.2P/server.js
@@ -13,6 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
     console.log('User connected');
 
+    socket.on('message', (msg) => {
+        console.log('Message received', msg);
+        io.emit('message', msg);
+    }
+    );
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
